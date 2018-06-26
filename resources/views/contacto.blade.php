0e85ebfad4fa2b9cf6289eb679e8977d331c8f0e
Nico <nicsojeda@gmail.com>
2018-06-25 21:19:00 -0300

    Test commit Fix

---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.app')
+ 
+@section('contacto')
+
+
+	<div class="consultanos" id="contacto">
+	   <h3>DEJANOS TU CONSULTA</h3>
+	   <hr>
+	   <form class="contact_form" action="" method="post" name="contact_form">
+	      <ul>
+	         <li>
+	            <input type="text" name="nombre" placeholder="*tu nombre" required/>
+	         </li>
+	         <li>
+	            <input type="text" name="email" placeholder="*tu email" required/>
+	         </li>
+	         <li>
+	            <textarea type="textarea" name="mensaje" placeholder="*tu mensaje"></textarea>
+	         </li>
+	         <li>
+	            <input type="submit" name="enviarcons" value="enviar" placeholder="ENVIAR">
+	         </li>
+	      </ul>
+	   </form>
+	</div>
+
+	<div class="newsleterr">
+		<h3>Sumate a nuestro newsletter</h3>
+		<hr>
+		<form class="contact_form" action="newslater.php" method="post" name="contact_form">
+	 		<ul>
+				<li>
+					<input type="text" name="email" placeholder="*tu email" required/>
+				</li>
+				<li>
+					<input type="submit" name="enviarnews" value="enviar" placeholder="ENVIAR">
+				</li>
+			</ul>
+		</div>	
+		</form>
+
+	</div>
+	<div class="ubicanos">
+		<h3>ACERCATE A NUESTRO LOCAL</h3>
+		<hr>
+		<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13136.066850016401!2d-58.3815704!3d-34.6037389!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x11bead4e234e558b!2sObelisco!5e0!3m2!1ses!2sar!4v1521055859994" width="100%" height="280px" frameborder="0" style="border: 4px solid #F2CD13" allowfullscreen></iframe>
+	</div>
+
+
+
+    
+    
+  @endsection
+
+
+
+    
+
